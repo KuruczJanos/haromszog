4ed8a1e2dad28573d171df8cc9f87cd00ef5d10d
Kurucz <kuruczjanoshivatalos@gmail.com>
2023-01-24 17:46:38 +0100

    web

---
+++ dist/app.js
@@ -0,0 +1,25 @@
+/*
+File: index.html
+Author: Kurucz János
+Copyright: 2023, Sallai András
+Group: Szoft V
+Date: 2023-01-17
+Github: https://github.com//
+Licenc: GNU GPL
+*/
+const baseElem = document.querySelector("#base");
+const heightElem = document.querySelector("#height");
+const calcButton = document.querySelector("#calcButton");
+const areaElem = document.querySelector("#area");
+
+calcButton.addEventListener('click', () => {
+    var base = Number(baseElem.value);
+    var height = Number(heightElem.value);
+    var area = calcTriangleArae(base, height);
+    console.log("Terület: " + area);
+    areaElem.value = area;
+});
+
+function calcTriangleArae(base, height) {
+    return base * height / 2;
+}
